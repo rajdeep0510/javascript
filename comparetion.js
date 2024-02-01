0d3c4b1d73546ccaefabc293dd359295dac736aa
rajdeep0510 <rajdeepvala10@gmail.com>
2024-02-01 22:53:43 +0530

    complited basic comparation operator

--- comparetion.js
+++ comparetion.js
@@ -5,3 +5,14 @@ console.log(2 >= 1);
 console.log(2 < 1);
 console.log(2 == 1);
 console.log(2 != 1);
+
+console.log("2" > 10); // not a good code
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+// Above given code is not a goo way to write therefore avoid it 
+
+//strict equality cheak 
+console.log("2" === 2); // three equal to is for cheaking the datatype as well as the value
