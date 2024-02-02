edf817d1929c2b8a6f3e78056123fdc60dc1fc69
rajdeep0510 <rajdeepvala10@gmail.com>
2024-02-02 16:26:59 +0530

    datatypes

---
+++ datatypes-notes.js
@@ -0,0 +1,34 @@
+// types of datatypes
+
+// primitive 
+
+//  7 bacic datatype => String, Number, Boolean, Null, Undifined, Symbol, BigInt
+
+
+//  Refrence (non-Primitive)
+//  3 types => Array, Object, Function 
+
+
+const score = 3
+const scoreValue = 100.33 // noneed of flote 
+
+const isLoogedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+
+// Arrays
+const names = ["Rajeep", "Shradhdha", "dev"]
+
+//object 
+let meObj = {
+    name: "rajdeep",
+    age: "23", // comma at the end 
+}
+const newFunction = function (){
+    console.log("hello world");
+}
