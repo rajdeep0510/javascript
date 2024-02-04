99d97289b7b3ff7c411b5c19e93932a400771103
rajdeep0510 <rajdeepvala10@gmail.com>
2024-02-04 17:17:47 +0530

    created 01.basics

---
+++ 01.basics/README.md
@@ -0,0 +1,3 @@
+# javascript
+This repository is used for keeping track of my JavaScript journey. <br>
+31 Jan I started javascript. 
---
+++ 01.basics/conversion.js
@@ -0,0 +1,32 @@
+let score = "33abc" //string 
+console.log(typeof score);
+
+let valueInNumber = Number(score) //convertsion
+console.log(typeof score);
+
+let mix = "33abc" // mixed value both num and letters
+let valueInMix= Number(mix)
+console.log(typeof valueInMix);
+console.log(valueInMix);
+ 
+ 
+// when converted
+// "22" => 22
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn);
+
+// 1 => true ; 0 => flase 
+// "raj" => true ; ""{empty} => fales 
+
+let num = 22
+let stringNumber = String(num) 
+console.log(stringNumber);
+console.log(typeof stringNumber); 
+// number converted to string 
+
+// syntax for conversion => let newVarable = name of datatype to be converted in to (varable)
+
