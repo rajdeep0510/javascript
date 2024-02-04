8e9cbd621c151c9e73577311fa64ee8075978678
rajdeep0510 <rajdeepvala10@gmail.com>
2024-02-04 22:50:58 +0530

    fixed d and t

--- 01.basics/date-time.js
+++ 01.basics/date-time.js
@@ -5,7 +5,7 @@ console.log(myDate.toString());  //Sun Feb 04 2024 17:47:37 GMT+0530 (India Stan
 console.log(myDate.toLocaleString()); //4/2/2024, 5:47:37 pm
 console.log(myDate.toLocaleTimeString()); //5:48:40 pm
 console.log(myDate.toLocaleDateString()); // 4/2/2024\
-console.log(myDate.to()); // 4/2/2024
+console.log(myDate.toTimeString()); // 4/2/2024
 
 let newDate = new Date(2024 ,0 ,23) //Get method 
 console.log(newDate.toLocaleString());
@@ -15,6 +15,3 @@ console.log(newDate.getMonth());
 
 let MyTimeStamp = new Date()
 console.log(Math.floor(MyTimeStamp.getTime()/1000));
-
-
- 
