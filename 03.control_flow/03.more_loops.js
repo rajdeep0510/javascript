ad856134fa0d64cf531aa106306e7b402162fd07
rajdeep0510 <rajdeepvala10@gmail.com>
2024-02-25 19:05:57 +0530

    more loops final "

---
+++ 03.control_flow/03.more_loops.js
@@ -0,0 +1,18 @@
+// for of 
+
+const arr = [1, 2, 3, 4, 5]
+for (const num of arr) {
+    console.log(num);
+}
+
+const greetings = "Hello World"
+for (const i of greetings) {
+    if(i ==" "){
+        continue
+    }
+    console.log(i);
+}
+ // Maps
+
+ const map = new Map()
+ map.s
